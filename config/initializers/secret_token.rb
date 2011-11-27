24f595a232736b6a39f8ad666edc6143c1d97e9c
B Kleinen <kleinen@infrastructure.de>
2011-11-27 15:40:13 +0100

    fresh rails app with html5 template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Html5primer::Application.config.secret_token = 'a27255e9332e5a83e1f01f1c5c5eeff7cef5143a9a8e64f8243eb915baf1a521fc434e4a88496016c69325a66ca5fcbb0f78e590dede6b889f415be9b67f94dd'
